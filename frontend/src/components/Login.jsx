f7500d8fe8f94af9521a0f7122a79d63798b9afd
AlanMontes <alanjms15@gmail.com>
2024-05-19 17:37:57 -0600

    fix route

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const RoutesearchLogin = process.env.VITE_SEARCHES_LOGIN;
+const RoutesearchLogin =  import.meta.env.VITE_SEARCHES_LOGIN || "http://localhost:8000/searches/login";
 
 
 
